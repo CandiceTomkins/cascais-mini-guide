e80e5ca24e5b6783cc3255193fd64c3a7c5dcedc
Candy Tomkins <candicetomkins@gmail.com>
2022-01-26 13:25:04 +0000

    updated project links, added Soi 55 link

--- src/Projects.js
+++ src/Projects.js
@@ -20,8 +20,12 @@ export default function Projects () {
 
           <h3>Alentejo, Portugal</h3>
           <p className="project-paragraph">Built with HTML, CSS and Javascript</p>
-          <a href="https://www.shecodes.io/workshops/shecodes-basics-275631ab-9840-4885-a6a8-5afa8911fe76/projects/434164" className="btn btn-project mb-3">see project</a>
+          <a href="https://www.shecodes.io/workshops/shecodes-basics-275631ab-9840-4885-a6a8-5afa8911fe76/projects/434164" className="btn btn-project mb-3" alt="alentejo page link">see project</a>
 
+
+          <h3>Soi 55 Lifestyle</h3>
+          <p className="project-paragraph">The Beach and Travel lifestyle / My Business</p>
+          <a href="https://soi55lifestyle.com/" className="btn btn-project mb-3" alt="soi 55 lifestyle link">see project</a>
                    </div>
                   
                </div>
