a731e606df03ba83a992be350611bcad5802407a
CandiceTomkins <candicetomkins@gmail.com>
2022-02-26 08:31:12 +0000

    init commit

--- src/App.js
+++ src/App.js
@@ -1,31 +1,67 @@
-import logo from './candy-profile.jpeg';
-import './App.css';
-import About from './About';
-import Projects from './Projects';
-import Contact from './Contact';
-import React from 'react';
-
+import heroImage from "./cascais-lighthouse.jpg";
+import "./App.css";
+import About from "./About";
+import Projects from "./Projects";
+import Contact from "./Contact";
+import React from "react";
 
 function App() {
   return (
     <div className="App">
       <div className="body-app">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      
-          <h1 className="my-name">Candice Tomkins <div className='front-end-dev'>Front-End Developer & UI Designer</div> <div className='based-in-lisbon'>based in Lisbon, Portugal 🇵🇹</div></h1>
-      </header>
+        <header className="App-header">
+          <img
+            src={heroImage}
+            className="hero-image"
+            alt="cascais lighthouse"
+          />
+
+          <h1 className="title">
+            Cascais, Portugal <div className="subtitle">A Mini Guide</div>
+          </h1>
+        </header>
       </div>
-      <About/>
-      <Projects/>
-      <Contact/>
-<footer><p>Coded by Candice Tomkins. Open-sourced on <a href="https://github.com/CandiceTomkins" alt="GitHub">GitHub</a><br/>
-<a href="mailto:candicetomkins@gmail.com" alt="email-address" className="social-link">Email</a>
-<a href="https://github.com/CandiceTomkins" alt="GitHub" className="social-link">GitHub</a>
-            <a href="https://www.linkedin.com/in/candice-tomkins-3b2129102" alt="linkedin-link" className="social-link">LinkedIn</a>
-            <a href="https://instagram.com/candytomkinstravels" alt="instagram-link" className="social-link">Instagram</a></p></footer>
+      <About />
+      <Projects />
+      <Contact />
+      <footer>
+        <p>
+          Coded by Candice Tomkins. Open-sourced on{" "}
+          <a href="https://github.com/CandiceTomkins" alt="GitHub">
+            GitHub
+          </a>
+          <br />
+          <a
+            href="mailto:candicetomkins@gmail.com"
+            alt="email-address"
+            className="social-link"
+          >
+            Email
+          </a>
+          <a
+            href="https://github.com/CandiceTomkins"
+            alt="GitHub"
+            className="social-link"
+          >
+            GitHub
+          </a>
+          <a
+            href="https://www.linkedin.com/in/candice-tomkins-3b2129102"
+            alt="linkedin-link"
+            className="social-link"
+          >
+            LinkedIn
+          </a>
+          <a
+            href="https://instagram.com/candytomkinstravels"
+            alt="instagram-link"
+            className="social-link"
+          >
+            Instagram
+          </a>
+        </p>
+      </footer>
     </div>
-    
   );
 }
 
